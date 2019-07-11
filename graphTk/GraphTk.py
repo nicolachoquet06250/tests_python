b27f2aa17ef67b15c27b1a0e9364860f46a7d8b3
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2019-07-11 16:51:15 +0200

    Ajout de toutes les options avec GraphicTk

--- graphTk/GraphTk.py
+++ graphTk/GraphTk.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Label
+from tkinter import Tk, Label, Button, TOP, LEFT, RIGHT, BOTTOM, Canvas, Frame, StringVar, Entry
 
 
 class GraphTk(object):
@@ -7,26 +7,46 @@ class GraphTk(object):
 	MENU = 'menu'
 	ROSASSE = 'rosasse'
 	CIBLE = 'cible'
+	MOVIES_LIST = 'my_movies_list'
+
+	GraphTop = TOP
+	GraphLeft = LEFT
+	GraphRight = RIGHT
+	GraphBottom = BOTTOM
 
 	@staticmethod
 	def add_type(type_window: str, callback: callable):
 		GraphTk.types[type_window] = callback
 
-	def __init__(self, type_window: str or None = None, create_window: bool = False):
+	def __init__(self, type_window: str or None = None, process: dict or None = None, create_window: bool = False):
 		self.window_type: str or None = None
 		self.root: Tk or None = Tk() if create_window else None
 		self.title: str or None = None
 		if type_window is not None:
 			self.__type__(type_window)
-			self.init()
+			if process is not None:
+				before_process = None
+				after_process = None
+				if "before" in process:
+					before_process = process["before"]
+				if "after" in process:
+					after_process = process["after"]
+				self.init(before_process, after_process)
+			else:
+				self.init()
 
-	def init(self):
-		self.process_init()
+	def init(self, before_process: callable or None = None, after_process: callable or None = None):
+		self.process_init(before_process, after_process)
 
-	def process_init(self):
-		current_local_window = GraphTk(None, True)
+	def process_init(self, before_process: callable or None = None, after_process: callable or None = None):
+		current_local_window = GraphTk(None, None, True)
 		current_local_window.__type__(self.window_type)
+		current_local_window.add_title(current_local_window.__is__())
+		if before_process is not None:
+			before_process()
 		GraphTk.types[self.window_type](current_local_window)
+		if after_process is not None:
+			after_process()
 
 	def __type__(self, type_window):
 		self.window_type = type_window
@@ -43,6 +63,28 @@ class GraphTk(object):
 	def add_title(self, title: str):
 		self.title = title
 		return self.root.title(title)
+
+	def add_button(self, text, command, side: str or None = None):
+		button = Button(self.root, text=text, command=command)
+		button.pack() if side is None else button.pack(side=side)
+
+	def add_canvas(self, bg, height=200, width=200, side: str or None = None, callback: callable or None = None):
+		canvas = Canvas(self.root, bg=bg, height=height, width=width)
+		if callback is not None:
+			canvas = callback(canvas)
+		canvas.pack() if side is None else canvas.pack(side=side)
+		return canvas
+
+	def add_frame(self, master=None, width=200, height=200, side: str or None = None, callback: callable or None = None):
+		frame = Frame(master=self.root if master is None else master, width=width, height=height)
+		if callback is not None:
+			frame = callback(frame, self)
+		frame.pack() if side is None else frame.pack(side=side)
+		return frame
+
+	def add_entry(self, default_text: str or None = None, default_variable_text: StringVar or None = None, width=200, side: str or None = None):
+		entry = Entry(self.root, text=default_text, textvariable=default_variable_text, width=width)
+		entry.pack() if side is None else entry.pack(side=side)
 
 	def get_title(self):
 		return self.title
--- tkinterNico/with_graphictk.py
+++ tkinterNico/with_graphictk.py
@@ -1,31 +1,156 @@
+from datetime import date
+from random import randrange
+from tkinter import StringVar, Frame, Label, Button
+
 from dsl.FilmManager import FilmManager
 from graphTk.GraphTk import GraphTk
 
 
+def quit_app(_):
+	_ensure = GraphTk(None, None, True)
+
+	def i_am_sure():
+		quit_win(_ensure)
+		quit_win(_)
+
+	def i_am_not_sure():
+		quit_win(_ensure)
+
+	_ensure.add_title("Êtes vous sûr ?")
+	_ensure.add_label("Voulez vous vraiment quitter l'application ?", GraphTk.GraphTop)
+	_ensure.add_button("Oui, je quitte !", command=i_am_sure, side=GraphTk.GraphLeft)
+	_ensure.add_button("Non, je ne veux pas quitter", command=i_am_not_sure)
+	_ensure.show()
+
+
+def quit_win(_):
+	if type(_) is GraphTk:
+		_.hide()
+	else:
+		_.quit()
+		_.destroy()
+
+
+def create_main_menu_buttons(_: GraphTk, side_return=None, side_quit=None):
+	_.add_button("Retour", command=lambda: GraphTk(
+		GraphTk.MENU, {"before": lambda: quit_win(_)}), side=side_return)
+	_.add_button("Quitter", command=lambda: quit_app(_), side=side_quit)
+
+
 def menu(window: GraphTk):
-	window.add_title(window.__is__())
-	print(window.get_title())
+	window.add_title("Menu")
+	window.add_button('Quitter', command=lambda: quit_app(window))
+	window.add_button('Déssiner une rosasse', command=lambda: GraphTk(
+		GraphTk.ROSASSE, {"before": lambda: quit_win(window)}))
+	window.add_button('Déssiner une cible', command=lambda: GraphTk(
+		GraphTk.CIBLE, {"before": lambda: quit_win(window)}))
+	window.add_button('Liste de films', command=lambda: GraphTk(
+		GraphTk.MOVIES_LIST, {"before": lambda: quit_win(window)}))
 	window.show()
 
 
 def rosasse(window: GraphTk):
-	window.root.title(window.__is__())
+
+	window.add_title("Rosasse")
+
+	def drowline(can1):
+		"""Tracé d'une ligne dans le canvas can1"""
+		global x1, y1, x2, y2, color
+		can1.create_line(x1, y1, x2, y2, width=2, fill=color)
+		y2, y1 = y2 + 10, y1 - 10
+
+	def changecolor():
+		"""changement aléatoir de couleur du tracé"""
+		global color
+		pal = ['purple', 'cyan', 'maroon', 'green', 'red', 'blue', 'orange', 'yellow']
+		c = randrange(8)
+		color = pal[c]
+
+	canvas = window.add_canvas('dark grey', 200, 200, GraphTk.GraphLeft)
+
+	create_main_menu_buttons(window)
+
+	window.add_button(text='Tracer une ligne', command=lambda: drowline(canvas))
+	window.add_button(text='Autre couleur', command=changecolor)
+
 	window.show()
 
 
 def cible(window: GraphTk):
-	window.root.title(window.__is__())
+
+	def create_canvas(canvas):
+		# create Y axe
+		canvas.create_line(100, 5, 100, (190 - 10) / 2)
+		canvas.create_line(100, (190 - 10) / 2 + 20, 100, 195)
+
+		# create X axe
+		canvas.create_line(5, 100, (190 - 10) / 2, 100)
+		canvas.create_line((190 - 10) / 2 + 20, 100, 195, 100)
+
+		# create cible circles
+		canvas.create_oval(10, 10, 190, 190)
+		canvas.create_oval(30, 30, 170, 170)
+		canvas.create_oval(50, 50, 150, 150)
+		canvas.create_oval(70, 70, 130, 130)
+		canvas.create_oval(90, 90, 110, 110)
+
+		return canvas
+
+	window.add_title("Cible")
+	window.add_canvas('dark grey', 200, 200, GraphTk.GraphLeft, create_canvas)
+	create_main_menu_buttons(window)
+	window.show()
+
+
+def movies_list(window: GraphTk):
+
+	def open_add_film_win(list_win: Frame):
+		_ = GraphTk(None, None, True)
+		_.add_title("Ajouter un film à la liste" + my_movies_list.title())
+
+		input_content = StringVar(_.root, '')
+		_.add_entry(None, input_content, 200, GraphTk.GraphTop)
+
+		def new_film():
+			my_movies_list.film().title(input_content.get()) \
+				.realisation_date(date.today()) \
+				.release_date(date.today()).build()
+			list_win.pack()
+
+		_.add_button(text="Valider", command=new_film, side=GraphTk.GraphBottom)
+		_.show()
+
+	def build_main_frame(frame: Frame, _: GraphTk):
+		my_list = Frame(frame)
+		for film in my_movies_list.films:
+			title = film.title()
+			text = Label(my_list, text=title)
+			text.pack(side=GraphTk.GraphTop)
+
+		form = Frame(master=frame, width=200, height=2)
+		add_film = Button(master=form, text="Ajouter Un film", command=lambda: open_add_film_win(my_list))
+		add_film.pack()
+		form.pack(side=GraphTk.GraphBottom)
+
+		my_list.pack()
+
+		return frame
+
+	window.add_title(my_movies_list.title())
+	window.add_frame(None, 200, 200, GraphTk.GraphTop, build_main_frame)
+	create_main_menu_buttons(window, GraphTk.GraphLeft, GraphTk.GraphRight)
 	window.show()
 
 
 if __name__ == '__main__':
 	x1, y1, x2, y2 = 10, 190, 190, 10
-	coul = 'dark green'
+	color = 'dark green'
 
-	liste = FilmManager().title("MCU")
+	my_movies_list = FilmManager().title("MCU")
 
 	GraphTk.add_type(GraphTk.MENU, callback=menu)
 	GraphTk.add_type(GraphTk.ROSASSE, callback=rosasse)
 	GraphTk.add_type(GraphTk.CIBLE, callback=cible)
+	GraphTk.add_type(GraphTk.MOVIES_LIST, callback=movies_list)
 
 	GraphTk(GraphTk.MENU)
